212e185d9fde048f4e5926850f3b73daf2b69f5a
Tejinder Purba <rikkupurba@gmail.com>
2021-08-19 18:48:53 +0000

    Updated sql scripts for bonds to be in the main database script

--- src/main/java/com/citi/training/portfolio/repo/BondRepository.java
+++ src/main/java/com/citi/training/portfolio/repo/BondRepository.java
@@ -66,11 +66,11 @@ public interface BondRepository extends JpaRepository<Bond, Integer> {
 
     /**
      * Retrieves the latest transaction of a specific bond.
-     * @param name the bond name to be fetched.
+     * @param issuer the bond issuer to be fetched.
      * @return The latest transaction for the specified bond.
      */
     @Query(
-            value = "SELECT * from bonds t1 where date_time = (select max(date_time) from bonds where bonds.name = :name) order by date_time desc",
+            value = "SELECT * from bonds t1 where date_time = (select max(date_time) from bonds where bonds.issuer = :issuer) order by date_time desc",
             nativeQuery = true)
-    Stock getLatestBondTransaction(@Param("name") String name);
+    Stock getLatestBondTransaction(@Param("issuer") String issuer);
 }
