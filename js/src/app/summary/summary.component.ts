be2a86d46858c61ab50413d01f2f983c8e325ca6
Tejinder Purba <rikkupurba@gmail.com>
2021-08-25 22:51:02 +0000

    Added summary transaction stuff

--- js/src/app/summary/summary.component.ts
+++ js/src/app/summary/summary.component.ts
@@ -37,6 +37,14 @@ export class SummaryComponent implements OnInit {
   isCashDepositTransactionCollapsed = true
   isCashWithdrawTransactionCollapsed = true
 
+  saleData = [
+    { name: "Mobiles", value: 105000 },
+    { name: "Laptop", value: 55000 },
+    { name: "AC", value: 15000 },
+    { name: "Headset", value: 150000 },
+    { name: "Fridge", value: 20000 }
+  ];
+
   constructor(private portfolioService:PortfolioService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
